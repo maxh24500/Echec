27ce91bf4d5ba5cd677ea19bfc3f6a09adf07097
Max Harrison <Max@zaretto.com>
2019-04-15 19:11:20 +0200

    latest version echecv6

--- Echecv4/Cavalier.h
+++ Echecv4/Cavalier.h
@@ -5,5 +5,5 @@
 class Cavalier : public Piece {
 public:
     Cavalier(int id, int positionX, int positionY, bool blanc);
-	virtual bool deplacement(int X, int Y);  
+	virtual bool deplacement(int X, int Y, Piece *unTableau);
 };
